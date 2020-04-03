d03432e1786988818ca0598238f0dadd243bb703
Jerry.zhao <jarray9@gmail.com>
2020-04-03 15:19:15 +0900

    Add download audio from youtube example

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 ## tqmd and ThreadPoolExecutor
    This example shows , how to display a progress bar on screen to show the status of threads.
    * [tqmd documentation](https://tqdm.github.io/docs/tqdm/)
+
+## use pafy to download audio from youtube
+   * [pafy documentation](https://pythonhosted.org/pafy/)
---
+++ creation/download-youtube-as-audio.py
@@ -0,0 +1,17 @@
+#!/bin/python
+'''
+pafyのdocumentation : https://pythonhosted.org/pafy/
+'''
+import sys
+import pafy
+
+
+def download_audio(url):
+    video = pafy.new(url)
+    for au in video.audiostreams:
+        au.download()
+
+if __name__ == '__main__':
+    if sys.argv[1:]:
+        for url in sys.argv[1:]:
+            download_audio(url)
